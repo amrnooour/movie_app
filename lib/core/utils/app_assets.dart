821415dd562f21429c493c03e785b9cb4d4766de
Your Name <you@example.com>
2024-06-25 12:00:10 +0300

    add assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,14 @@
+abstract class AppAssets {
+  static const splash = "assets/splash.png";
+  static const homeIcon = "assets/Home icon.png";
+  static const homeSelected = "assets/Home selected.png";
+  static const searchIcon = "assets/search.png";
+  static const searchSelected = "assets/search selected.png";
+  static const watchListIcon = "assets/watch list.png";
+  static const watchListSelected = "assets/watch list selected.png";
+  static const browseIcon = "assets/browse.png";
+  static const browsrSelected = "assets/browse selected.png";
+  static const image = "assets/image.png";
+
+
+}
